5c480a742bb041d9317e31aef06150751efd3304
raulguzman20 <raulguzmaz1023@gmail.com>
2024-09-16 11:10:55 -0500

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import Server from './models/server.js'; // Asegúrate de que la ruta es correcta
 
 const server = new Server();
- 
+// link render cliente  https://taller-api-clientes.onrender.com/api/clientes
+
+//link render usuarios  https://taller-api-clientes.onrender.com/api/usuarios
+
