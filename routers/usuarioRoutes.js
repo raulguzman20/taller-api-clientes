86b24f048f8863983e296702e20ea6fc5f1ce93d
raulguzman20 <raulguzmaz1023@gmail.com>
2024-09-17 10:16:43 -0500

    usuarioRoutes.js

--- routers/usuarioRoutes.js
+++ routers/usuarioRoutes.js
@@ -1,11 +1,18 @@
-import express from 'express';
+import { Router } from 'express';
 import { getUsuarios, postUsuario, putUsuario, deleteUsuario } from '../controllers/usuarioController.js';
 
-const router = express.Router();
+const router = Router();
+
 
 router.get('/', getUsuarios);
+
+
 router.post('/', postUsuario);
+
+
 router.put('/:id', putUsuario);
+
+
 router.delete('/:id', deleteUsuario);
 
 export default router;
